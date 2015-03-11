bd1b9069aaf401cb048b52d212bbc313018d3388
Ferdinand von den Eichen <ferdinand@vondeneichen.email>
2015-03-11 12:19:21 +0100

    DataService, Databinding, Structural changes

---
+++ funtastic/funtastic.Shared/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace funtastic.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ funtastic/funtastic.Shared/ViewModels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using funtastic.DataModel;
+using funtastic.Services;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace funtastic.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private MobileServiceCollection<Joke, Joke> best;
+        public MobileServiceCollection<Joke, Joke> Best
+        {
+            get { return best; }
+            set
+            {
+                best = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private String test;
+        public String Test
+        {
+            get { return test; }
+            set
+            {
+                test = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Initialize();
+        }
+
+        public async void Initialize()
+        {
+            await DataService.Instance.Initialize();
+
+            // Needs to be called after Initialization
+            DataService.Instance.RegisterDataSources(ref best);
+            Best = best;
+
+            Test = "#yolo";
+        }
+    }
+}
