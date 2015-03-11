c79b309b9f310204a26cd870f15a17d53ac77f4e
Ferdinand <Ferdinand@localhost>
2015-03-11 12:19:21 +0100

    DataService, Databinding, Structural changes

---
+++ funtastic/funtastic.Shared/DataModel/Joke.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace funtastic.DataModel
+{
+    public class Joke
+    {
+        public string Id { get; set; }
+
+        private string content;
+        [JsonProperty(PropertyName = "content")]
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        private string authorId;
+        [JsonProperty(PropertyName = "authorId")]
+        public string AuthorId
+        {
+            get { return authorId; }
+            set
+            {
+                authorId = value;
+            }
+        }
+
+        private int rating;
+        [JsonProperty(PropertyName = "rating")]
+        public int Rating
+        {
+            get { return rating; }
+            set
+            {
+                rating = value;
+            }
+        }
+
+        private int reports;
+        [JsonProperty(PropertyName = "reports")]
+        public int Reports
+        {
+            get { return reports; }
+            set
+            {
+                reports = value;
+            }
+        }
+
+        [JsonProperty(PropertyName = "timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [JsonProperty(PropertyName = "language")]
+        public string Language { get; set; }
+
+        public Joke()
+        {
+            this.Rating = 0;
+            this.Reports = 0;
+            this.Content = "";
+            this.AuthorId = "";
+            this.Timestamp = DateTime.Now;
+            this.Language = System.Globalization.CultureInfo.CurrentCulture.EnglishName;
+        }
+    }
+}
---
+++ funtastic/funtastic.Shared/Services/DataService.cs
@@ -0,0 +1,144 @@
+﻿using funtastic.DataModel;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace funtastic.Services
+{
+    public class DataService
+    {
+        // Current in memory items
+        private MobileServiceCollection<Joke, Joke> best;
+
+        // Table items (both local and remote)
+        private IMobileServiceSyncTable<Joke> jokeTable;
+
+        private static DataService instance;
+
+        public bool IsInitialized { get; set; }
+
+        private DataService()
+        {
+            jokeTable = App.MobileService.GetSyncTable<Joke>();
+        }
+
+        public static DataService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataService();
+                }
+                return instance;
+            }
+        }
+
+        public async Task Initialize()
+        {
+            if (!IsInitialized)
+            {
+                await InitLocalStoreAsync();
+                await RefreshJokes();
+                IsInitialized = true;
+            }
+        }
+
+        public void RegisterDataSources(ref MobileServiceCollection<Joke, Joke> bestRef)
+        {
+            if (!IsInitialized)
+            {
+                throw new Exception("This may not be called before initialization!)");
+            }
+
+            bestRef = best;
+        }
+
+        public async Task AddJoke(Joke item)
+        {
+            await jokeTable.InsertAsync(item);
+            best.Add(item);
+
+            await SyncAsync(); // offline sync
+        }
+
+        public async Task RateJoke(Joke item)
+        {
+
+        }
+        public async Task ReportJoke(Joke item)
+        {
+
+        }
+
+
+        private async Task RefreshJokes()
+        {
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                best = await jokeTable
+                    .ToCollectionAsync();
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                exception = e;
+            }
+
+            if (exception != null)
+            {
+                await new MessageDialog(exception.Message, "Error loading items").ShowAsync();
+            }
+        }
+
+        #region Offline Sync
+
+        private async Task InitLocalStoreAsync()
+        {
+            if (!App.MobileService.SyncContext.IsInitialized)
+            {
+                var store = new MobileServiceSQLiteStore("localstore.db");
+                store.DefineTable<Joke>();
+                await App.MobileService.SyncContext.InitializeAsync(store);
+            }
+
+            await SyncAsync();
+        }
+
+        private async Task SyncAsync()
+        {
+            String errorString = null;
+
+            try
+            {
+                await App.MobileService.SyncContext.PushAsync();
+                await jokeTable.PullAsync("joke", jokeTable.CreateQuery()); // first param is query ID, used for incremental sync
+            }
+
+            catch (MobileServicePushFailedException ex)
+            {
+                errorString = "Push failed because of sync errors: " +
+                  ex.PushResult.Errors.Count + " errors, message: " + ex.Message;
+            }
+            catch (Exception ex)
+            {
+                errorString = "Pull failed: " + ex.Message +
+                  "\n\nIf you are still in an offline scenario, " +
+                  "you can try your Pull again when connected with your Mobile Serice.";
+            }
+
+            if (errorString != null)
+            {
+                MessageDialog d = new MessageDialog(errorString);
+                await d.ShowAsync();
+            }
+        }
+
+        #endregion
+    }
+}
